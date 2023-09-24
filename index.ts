89ba2e7b25c37ebce1fa01531d8a662e392aef2a
GeorgeRNG <grogrng@gmail.com>
2023-09-24 14:10:51 +0100

    loads of stuff

---
+++ index.ts
@@ -0,0 +1,31 @@
+import { env } from "bun";
+import Elysia from "elysia";
+import { setUser } from "./store";
+
+
+type Store = Record<string, string | null>;
+function isValidPlot(req: {'headers': Store, 'query': Store}) {
+    if(req.query['auth'] != env.AUTH_KEY) return false;
+    const match = (req.headers['user-agent']?.match(/DiamondFire\/(?<version>\d(?:\.\d)+) \((?<plotid>\d+), (?<plotowner>\w{3,16})\)/));
+    if(match == null) return false;
+    const [matched, dfversion, plotid, plotowner] = match;
+    if(plotid != env.PLOT_ID) return false;
+    
+    if(plotowner != env.PLOT_OWNER) return false;
+    return true;
+}
+
+new Elysia()
+    .get('/', () => `Hello World!`)
+    .put('/add', async (req) => {
+        if(!isValidPlot(req)) {
+            req.set.status = "Forbidden";
+            // log the requesters ip 🤨📸
+            // for legal reasons this is a joke.
+            return "403 Forbidden.";
+        }
+        const {uuid, username, key} = req.body as any; // we will error, and we will cope, and we will seethe, and we will cope, and we will seethe
+        setUser(uuid, username, key)
+        return; // Only DF is supposed to be here, and it doesn't use response bodies.
+    })
+    .listen(3000)
---
+++ store.ts
@@ -0,0 +1,46 @@
+import { env, sha } from "bun";
+import { Database } from 'bun:sqlite'
+
+
+const IS_DETA = env.DETA == 'true';
+async function load() {
+    if(IS_DETA) {
+        const res = await (await import('deta')).Drive('dfonline-oauth').get(env.DETA_PATH ?? 'database.db');
+        return Database.deserialize( await res?.arrayBuffer() ?? new ArrayBuffer(0))
+    }
+    else {
+        return new Database(env.DB_PATH, { create: true });
+    }
+}
+async function save() {
+    if(IS_DETA) {
+        const res = await (await import('deta')).Drive('dfonline-oauth').put(env.DETA_PATH ?? 'database.db', { data: db.serialize() })
+    }
+    else {
+        // tmk it saves anyway
+        // :memory: enjoyers
+        // time to for universe to collapse when someone uses my code in production in a critical scenario and have important data lost to this
+        // "sir, what happened to the database" "well, have a look at this"
+        // "what the f--"
+    }
+}
+
+const db = await load();
+db.run(`CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY,
+    username VARCHAR(16),
+    token TEXT
+);`)
+
+
+/**
+ * Securely saves a user. Will hash the key, so don't hash it before this.
+ * @param uuid Minecraft UUID
+ * @param username Minecraft Name
+ * @param key Non-Hashed pure access key.
+ */
+export function setUser(uuid: string, username: string, key: string) {
+    const hasher = new Bun.CryptoHasher("sha256");
+    hasher.update(key);
+    db.query(`INSERT INTO users (id, username, token) VALUES (?, ?, ?);`).run(uuid,username,hasher.digest('base64'));
+}
